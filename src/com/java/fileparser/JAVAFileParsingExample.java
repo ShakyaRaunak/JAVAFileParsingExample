3fe296ba55cf5b322f78bc477e9dd6b45bb6b213
Raunak Shakya <rkshakya99@gmail.com>
2015-06-18 22:53:57 +0545

    different ways of declaring an Array

--- src/com/java/fileparser/JAVAFileParsingExample.java
+++ src/com/java/fileparser/JAVAFileParsingExample.java
@@ -13,43 +13,67 @@ import java.util.*;
 public class JAVAFileParsingExample {
 
     public static void main(String[] args) throws IOException, ParseException {
-        // write your code here
 
-        String[] myArray = new String[]{"a", "b"};
-        /*int lengthOfArray = myArray.length;
-        for(int i=0;i<lengthOfArray;i++) {
-            System.out.println(myArray[i]);
-        }*/
-        /*for(String a:myArray) {
+        // Different ways of creating an Array
+        /*
+        String[] countries = new String[]{"Australia", "New Zealand", "South Africa", "England"};
+        */
+
+        /*
+        String[] countries = new String[4];
+        countries[0] = "Australia";
+        countries[1] = "New Zealand";
+        countries[2] = "South Africa";
+        countries[3] = "England";
+        */
+
+        /*
+        String[] countries = {"Australia", "New Zealand", "South Africa", "England"};
+        */
+
+        String[] countries = {"Australia", "New Zealand", "South Africa", "England"};
+        int lengthOfArray = countries.length;
+
+        // Printing elements of Array using for loop
+        /*
+        for (int i = 0; i < lengthOfArray; i++) {
+            System.out.println(countries[i]);
+        }
+        */
+
+        // Printing elements of Array using for-each loop
+        for (String a : countries) {
             System.out.println(a);
-        }*/
+        }
+        System.out.println();
+
+        // Using ArrayList
         ArrayList al = new ArrayList();
-        /*al.add("Ravi");
-        al.add("Vijay");
-        al.add("Ravi");
-        al.add("Ajay");*/
-        al.add("Ravi");
-        al.add("Vijay");
-        al.add("Ravi");
-        al.add("Ajay");
+        al.add("Australia");
+        al.add("New Zealand");
+        al.add("South Africa");
+        al.add("England");
         Collections.sort(al);
+        Iterator itr1 = al.iterator();
+        while (itr1.hasNext()) {
+            System.out.println(itr1.next());
+        }
+        System.out.println();
 
-        Iterator itr = al.iterator();
-        /*while (itr.hasNext()) {
-            System.out.println(itr.next());
-        }*/
-
+        // Using LinkedHashMap
         LinkedHashMap hm = new LinkedHashMap();
-        hm.put(100, "Amit");
-        hm.put(101, "Vijay");
-        hm.put(102, "Rahul");
+        hm.put(100, "Australia");
+        hm.put(101, "New Zealand");
+        hm.put(102, "South Africa");
         Set set = hm.entrySet();
-        /*Iterator itr = set.iterator();
-        while(itr.hasNext()) {
-            Map.Entry m = (Map.Entry) itr.next();
+        Iterator itr2 = set.iterator();
+        while (itr2.hasNext()) {
+            Map.Entry m = (Map.Entry) itr2.next();
             System.out.println(m.getKey() + " " + m.getValue());
-        }*/
+        }
+        System.out.println();
 
+        /*
         MemberFileConverter memberFileConverter = new MemberFileConverter() {
             @Override
             public boolean isEligible(Eligibility eligibility) {
@@ -57,8 +81,10 @@ public class JAVAFileParsingExample {
             }
         };
         memberFileConverter.convert(FileUtils.MEMBERS_FILE, FileUtils.ELIGIBILITIES_FILE, FileUtils.OUTPUT_FILE);
+        */
 
         /*
+        // Question from Facebook
         String r = "0";
         int x = -1, y = -5;
         if(x < 5)
